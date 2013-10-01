3ad0a7270de6c4bd0d0586cffd49ad6033a09a62
Brian Fincher <brian@fincherhome.com>
2013-10-01 11:02:56 -0500

    fix errors

--- thumbnail_py/thumbnail.py
+++ thumbnail_py/thumbnail.py
@@ -26,13 +26,13 @@ class Thumbnail():
 	if name == "lost+found":
 	    return False
 
-        if os.path.isdir(dir + "/" + name):
+        if os.path.isdir(os.path.join(dir, name)):
 	    return name != "thumbs"
 	else:
 	    if imagePattern.match(name):
 	        thumbsDir = self.getThumbsDir(dir)
 	        if os.path.exists(thumbsDir) and os.path.isdir(thumbsDir):
-		    return not os.path.exists(thumbsDir + "/" + name)
+		    return not os.path.exists(os.path.join(thumbsDir, name))
 		else:
 		    return True
 
@@ -41,7 +41,7 @@ class Thumbnail():
     def getThumbsDir(self, dir):
         path = dir.replace(self.basePath, "")
 	if len(path) > 0:
-	    thumbsDir = self.baseThumbsDir + "/" + path
+	    thumbsDir = os.path.join(self.baseThumbsDir, path)
 	else:
 	    thumbsDir = self.baseThumbsDir
 
@@ -51,66 +51,68 @@ class Thumbnail():
 	return thumbsDir
 
     def processDir(self, dir):
+        #print "processing dir %s" % dir
         for f in os.listdir(dir):
 	    if not self.acceptFile(dir, f):
 	        continue
 
-	    file = dir + "/" + f
+	    file = os.path.join(dir, f)
             if os.path.isdir(file):
                 self.processDir(file)
             else:
-	        print "Processing %s" % file
+                print "Processing %s" % file
                 try:
                     im = Image.open(file)
                     im.thumbnail((150, 150), Image.ANTIALIAS)
 
-	            thumbsDir = self.getThumbsDir(dir)
-	            destFile = thumbsDir + "/" + f
-	            extension = getFileExtension(f)[1:]
+                    thumbsDir = self.getThumbsDir(dir)
+                    destFile = os.path.join(thumbsDir, f)
+                    extension = getFileExtension(f)[1:]
 
                     im.save(destFile,  imageTypes[extension])
                 except IOError as e:
                     print e
 
     def renameLargeFiles(self, dir):
+        print "renameLargeFiles processing dir %s" % dir
         count = 1
-	for root, dirs, files in os.walk(dir):
-	    for f in files:
-	        if not self.acceptFile(root, f):
-	            continue
+        for root, dirs, files in os.walk(dir):
+            for f in files:
+                if not self.acceptFile(root, f):
+                    continue
 
-	        file = root + "/" + f
-	        if os.path.isdir(file):
-	            self.renameLargeFiles(file)
-	        elif len(f) > 15:
-	            while True:
-		        newFileName = "IMG_"
+                file = os.path.join(root, f)
+                if os.path.isdir(file):
+                    self.renameLargeFiles(file)
+                elif len(f) > 15:
+                    while True:
+                        newFileName = "IMG_"
                         zeroes = 4 - int(math.log(count, 10))
-		        for i in range(zeroes):
-		            newFileName = newFileName + "0"
-		        newFileName = newFileName + str(count)
-		        count +=1
+                        for i in range(zeroes):
+                            newFileName = newFileName + "0"
+                        newFileName = newFileName + str(count)
+                        count +=1
 
-		        extension = getFileExtension(f)
-		        newFileName = newFileName + extension
+                        extension = getFileExtension(f)
+                        newFileName = newFileName + extension
 
-		        if not os.path.exists:
-		            break
+                        if not os.path.exists(newFileName):
+                            break
 
-		    print "Renaming %s to $s" % (f, newFileName)
-		    os.rename(file, root + "/" + newFileName)
+                    print "Renaming %s to %s" % (f, newFileName)
+                    os.rename(file, os.path.join(root, newFileName))
 	        
 
 
 def deleteObsoleteThumbs(picDir, thumbDir):
     for file in os.listdir(thumbDir):
-        thumbFile = thumbDir + "/" + file
-        picFile = picDir + "/" + file
+        thumbFile = os.path.join(thumbDir, file)
+        picFile = os.path.join(picDir, file)
         if os.path.exists(picFile):
 	    if os.path.isdir(picFile):
 	        deleteObsoleteThumbs(picFile, thumbFile)
 	else:
-	    print "Deleting " + thumbFile
+	    print "Deleting %s" % thumbFile
 	    if os.path.isdir(thumbFile):
 	        shutil.rmtree(thumbFile)
 	    else:
@@ -129,8 +131,11 @@ def main(argv):
 
     print "basePath = %s, baseThumbsDir = %s" % (basePath, baseThumbsDir)
 
+    print "renaming large file names"
     thumb.renameLargeFiles(baseDir)
+    print "processing %s" % baseDir
     thumb.processDir(baseDir)
+    print "deleting obsolete thumbs"
     deleteObsoleteThumbs(basePath, baseThumbsDir)
 
 
